8f5b52265f2db980fbac2168dc5038cecfa18e0c
SasangaME <madhumaledirisinghe@gmail.com>
2023-10-09 23:31:03 +0530

    Unit test setup

--- CashFlowApp.BusinessLogic.Tests/Services/CategoryServiceTests.cs
+++ CashFlowApp.BusinessLogic.Tests/Services/CategoryServiceTests.cs
@@ -1,15 +1,46 @@
 namespace CashFlowApp.BusinessLogic.Tests;
 
+using AutoMapper;
+using CashFlowApp.BusinessLogic.Services;
+using CashFlowApp.Models.Entities;
+using CashFlowApp.Models.Mappings;
 using CashFlowApp.Repositories.Repos;
 using Moq;
 
 public class CategoryServiceTests
 {
-    private readonly Mock<ICategoryRepository> _categoryRepository;
+    private readonly Mock<ICategoryRepository> _mockRepository;
+    private readonly IMapper _mapper;
 
     public CategoryServiceTests()
     {
-        
+        _mockRepository = new Mock<ICategoryRepository>();
+        var mapperConfig = new MapperConfiguration(config
+            =>
+        {
+            config.AddProfile(new ApplicationProfile());
+        });
+        _mapper = mapperConfig.CreateMapper();
     }
-    
+
+    private IEnumerable<Category> GetCategories()
+    {
+        List<Category> categories = new();
+        categories.Add(new Category() { Id = 1, Name = "Category 1", Description = "Test 1" });
+        categories.Add(new Category() { Id = 2, Name = "Category 2", Description = "Test 2" });
+        return categories;
+    }
+
+    [Fact]
+    public async Task GetAllCategories_ReturnCategoryList()
+    {
+        var data = GetCategories();
+        _mockRepository.Setup(m => m.FindAll()).ReturnsAsync(data);
+        ICategoryService categoryService = new CategoryService(_mockRepository.Object, _mapper);
+        
+        var result = await categoryService.FindAll();
+        
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Count());
+    }
 }
