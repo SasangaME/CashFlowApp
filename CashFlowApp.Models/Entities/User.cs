4cde7c6e868fa2b21971683e23c376b11bcd21bb
SasangaME <madhumaledirisinghe@gmail.com>
2023-10-13 23:39:14 +0530

    created User entity and added migration

--- CashFlowApp.Models/Entities/User.cs
+++ CashFlowApp.Models/Entities/User.cs
@@ -6,5 +6,6 @@ public class User : BaseEntity
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public string Role { get; set; } = string.Empty;
+    public int RoleId { get; set; }
+    public Role Role { get; set; } = new Role();
 }
